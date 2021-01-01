113e2f83104214986ddf70e6fe3142032640067b
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2021-01-01 21:09:32 +0500

    lesson №31

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,6 +8,13 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
     let messagesElements = props.state.messages.map(m => <Message message={m.message} />);
 
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        let message = newMessageElement.current.value;
+        alert(message);
+    }
+
     return (
         <div className={D.dialogs}>
             <div className={D.dialogsItems}>
@@ -15,6 +22,12 @@ const Dialogs = (props) => {
             </div>
             <div className={D.messages}>
                 {messagesElements}
+                <div>
+                    <textarea ref={newMessageElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addMessage}>Add message</button>
+                </div>
             </div>
         </div>
     )
