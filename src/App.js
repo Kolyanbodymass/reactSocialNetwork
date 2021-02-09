0daa98cee7d53a738c428af2d519a39a932c17b0
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2021-02-09 11:28:11 +0500

    lesson 99 (router switch, try-catch, classnames)

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navbar from './components/Navbar/Navbar.jsx';
 import News from './components/News/News.jsx';
 import Music from './components/Music/Music.jsx';
 import Settings from './components/Settings/Settings.jsx';
-// import { BrowserRouter, Route, withRouter } from 'react-router-dom';
-import { HashRouter, Route, withRouter } from 'react-router-dom';
+// import { BrowserRouter, Route, withRouter, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import UsersContainer from './components/Users/UsersContainer.jsx';
 import LoginPage from './components/Login/Login';
 import {connect} from 'react-redux';
@@ -26,9 +26,18 @@ const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileCo
 
 class App extends React.Component {
 
+	catchAllUnhandledErrors = (promiseRejectionEvent) => {
+		alert(promiseRejectionEvent);
+	}
+
 	componentDidMount() {
         this.props.initializeApp();
+		window.addEventListener("unhandledrejection", this.catchAllUnhandledErrors);
     }
+
+	componentWillUnmount() {
+		window.removeEventListener("unhandledrejection", this.catchAllUnhandledErrors);
+	}
 
 	render() {
 		if (!this.props.initialized) {
@@ -40,20 +49,26 @@ class App extends React.Component {
 				<HeaderContainer />
 				<Navbar friends={this.props.friends}/>
 				<div className="app-wrapper-content">
-					<Route path="/profile/:userId?" 
-						render={ withSuspense(ProfileContainer) } />
-					<Route path="/dialogs" 
-						render={ withSuspense(DialogsContainer) } />
-					<Route path="/news" 
-						render={ () => <News /> } />
-					<Route path="/music" 
-						render={ () => <Music /> } />
-					<Route path="/settings" 
-						render={ () => <Settings /> } />
-					<Route path="/users" 
-						render={ () => <UsersContainer /> } />
-					<Route path="/login" 
-						render={ () => <LoginPage /> } />
+					<Switch>
+						<Route exact path="/" 
+							render={ () => <Redirect to={"/profile"} />}/>
+						<Route path="/profile/:userId?" 
+							render={ withSuspense(ProfileContainer) } />
+						<Route path="/dialogs" 
+							render={ withSuspense(DialogsContainer) } />
+						<Route path="/news" 
+							render={ () => <News /> } />
+						<Route path="/music" 
+							render={ () => <Music /> } />
+						<Route path="/settings" 
+							render={ () => <Settings /> } />
+						<Route path="/users" 
+							render={ () => <UsersContainer /> } />
+						<Route path="/login" 
+							render={ () => <LoginPage /> } />
+						<Route path="*" 
+							render={ () => <div>404 NOT FOUND</div> } />
+					</Switch>
 				</div>
 			</div>
 		);
