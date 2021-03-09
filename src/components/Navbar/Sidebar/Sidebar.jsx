2ff8923c77074d9f83146630d113fb6af46d3b3e
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2021-03-09 20:06:43 +0500

    added css styles for sidebar

--- src/components/Navbar/Sidebar/Sidebar.jsx
+++ src/components/Navbar/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import S from './Sidebar.module.css';
+import styles from './Sidebar.module.css';
 import SidebarFriend from './SidebarFriend/SidebarFriend.jsx';
 
 const Sidebar = (props) => {
@@ -7,9 +7,11 @@ const Sidebar = (props) => {
     let SidebarFriendElements = props.friends.map( f => <SidebarFriend src={f.src} name={f.name} key={f.id} id={f.id} /> );
 
     return (
-       <div className={S.sidebar}>
-           <div>Friends:</div>
-           {SidebarFriendElements}
+       <div className={styles.sidebar}>
+            <div className={styles.title}>Friends:</div>
+            <div className={styles.itemsWrapper}>
+                {SidebarFriendElements}
+            </div>
        </div> 
     );
 }
--- src/redux/sidebar-reducer.js
+++ src/redux/sidebar-reducer.js
@@ -3,6 +3,9 @@ let initialState = {
         { id: 1, name: 'Kolya', src: 'https://g0.sunmag.me/sunmag.me/wp-content/uploads/2019/11/sunmag-004-small-avatar.png' },
         { id: 2, name: 'Katya', src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOfqBBnGW-ntm2BhRJQQJupmw5Gh5drIDnvA&usqp=CAU' },
         { id: 3, name: 'Vova', src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7wIM1l36lND_jsEwki3UCnRpcTzs-AfPv-A&usqp=CAU' },
+        { id: 4, name: 'Sveta', src: 'https://g0.sunmag.me/sunmag.me/wp-content/uploads/2019/11/sunmag-004-small-avatar.png' },
+        { id: 5, name: 'Sergey', src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOfqBBnGW-ntm2BhRJQQJupmw5Gh5drIDnvA&usqp=CAU' },
+        { id: 6, name: 'Mysha', src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7wIM1l36lND_jsEwki3UCnRpcTzs-AfPv-A&usqp=CAU' },
     ]
 };
 
